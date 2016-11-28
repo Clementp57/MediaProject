581eaefff001c428954a0ba33fe37f88dd7f289d
Clément Peyrabere <Clem@MacBook-Pro-de-Clement-3.local>
2016-11-28 12:00:21 +0100

    degbug

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -25,6 +25,7 @@ router.post('/', function (req, res) {
     // every time a file has been uploaded successfully,
     // rename it to it's orignal name
     form.on('file', function (field, file) {
+        console.log("FILE => ", file);
         uploads.push(file);
     });
 
@@ -36,12 +37,15 @@ router.post('/', function (req, res) {
     // once all the files have been uploaded, send a response to the client
     form.on('end', function () {
         uploads.forEach((upload) => {
+            console.log("UPLOAD => ", upload);
             var media = new Media();
             var fileExtension = path.extname(upload.name);
             media.name = upload.name.split(fileExtension)[0];
             media.size = upload.size;
             media.mimeType = upload.type;
             media.fullPath = path.join(form.uploadDir, upload.name);
+
+            console.log("MEDIA => ", media);
 
             fs.rename(upload.path, media.fullPath);
             media.save((result) => {
